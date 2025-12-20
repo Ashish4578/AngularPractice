573c558db4cb34ba28700ba36f22e45623c88eaa
Ashish4578 <ashishgadekara1541@gmail.com>
2025-12-20 21:43:30 +0530

    done calculator task

--- myProject01/src/app/app.component.ts
+++ myProject01/src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'myProject01';
+  name = 'Ashish Gadekar';
+  num1: number =0;
+  num2: number =0;
+  result: number  | string = 0 ;
+
+  add(){
+    this.result = this.num1 + this.num2;
+  }
+  subtract(){
+    this.result = this.num1 - this.num2;
+  }
+  multiply(){
+    this.result = this.num1 * this.num2;
+  }
+  divide(){
+    if(this.num2 != 0){
+      this.result = this.num1 / this.num2;
+    } else {
+      this.result = 'Error: Division by zero';
+    }
+  }
+
 }
